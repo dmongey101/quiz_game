b0484af6dcb2a4fda75e81d32618beb2e174ed7c
Donal Mongey <donalmongey@gmail.com>
2018-10-11 14:01:02 +0000

    Ending Game

--- quiz.py
+++ quiz.py
@@ -1,9 +1,26 @@
-print("Quiz Game")
-print("----------")
-# print()
-print("1. Run Quiz")
-print("2. Enter a Question")
-print("3. Exit")
-print()
+def show_menu():
+    print("Quiz Game")
+    print("----------")
+    print("1. Run Quiz")
+    print("2. Enter a Question")
+    print("3. Exit")
+    print()
+    
+    option = int(input("Enter Option: "))
+    return option
 
-option = int(input("Enter Option: "))
+
+while True:    
+    option = show_menu()
+    if option == 3:
+        print("Game ended")
+        break
+    
+    
+    
+    
+    
+    
+
+
+
